c3969eea2787adb9e10c6dbbaca996468b8c55a5
Adhithyan Sakthivelu <admkr.2010@gmail.com>
2022-06-02 23:30:33 -0700

    added problem instance

--- problem_instance_1.py
+++ problem_instance_1.py
@@ -4,6 +4,7 @@ from car_driver_data import *
 from master_problem import *
 from optimal_rollouts import *
 from distance_matrix_API import *
+from optimal_dropoffs import *
 
 new_cust = "37.429070511839086,-122.16979819313977"
 location_list =  generate_cars(20)
@@ -22,9 +23,12 @@ for i in range(len(feasible_cars_list)):
     T_car, D_car = createDistanceMatrix(feasible_cars_list[i])
     energy_matrix_dict[feasible_cars_keys[i]] = get_energy_matrix(T_car, D_car)
 
-
+sample_energy = []
 for energy_matrix in energy_matrix_dict:
     print(energy_matrix)
     print(energy_matrix_dict[energy_matrix].shape)
     print(energy_matrix_dict[energy_matrix])
+    sample_energy = energy_matrix_dict[energy_matrix]
 
+
+drop_order(sample_energy)
